204953bbbf233655f5c5b412d29bfa89119e93c0
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-10-16 05:58:11 -0400

    final changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-    { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-    { path: 'features', loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule) }
+    { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -6,8 +6,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
     form: FormGroup;
@@ -17,8 +16,8 @@ export class LoginComponent implements OnInit {
 
     ngOnInit() {
         this.form = this.fb.group({
-            'username': ['', Validators.compose([Validators.required, Validators.minLength(1)])],
-            'password': ['', Validators.compose([Validators.required, Validators.minLength(1)])]
+            'username': ['', Validators.compose([Validators.required])],
+            'password': ['', Validators.compose([Validators.required])]
         });
     }
 
