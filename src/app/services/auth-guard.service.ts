1d5054c53825ba35ccaf985ef14b9659abde94a3
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-10-31 10:19:06 -0400

    Latest updates

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -6,7 +6,7 @@ import { AuthenticationService } from './authentication.service';
 export class AuthGuard implements CanActivate {
     constructor(private router: Router, private authenticationService: AuthenticationService) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : boolean {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
         const isLoggedIn = this.authenticationService.isLoggedIn();
 
         if (isLoggedIn) {
