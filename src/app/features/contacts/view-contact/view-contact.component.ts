b397b670e70d661bf6f3b4f3bd5a231344118794
Brian Carey <careyb@mindspring.com>
2021-01-25 17:04:23 -0500

    VIewing an individual contact

--- src/app/features/contacts/view-contact/view-contact.component.ts
+++ src/app/features/contacts/view-contact/view-contact.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ContactService } from '../../service/contact.service';
 import { Contact } from '../models/contact';
 import { AlertService } from '../../../ui/alert/alert.service';
 import { HttpErrorResponse } from '@angular/common/http';
-
+import { Router } from '@angular/router';
+import { phoneTypes } from '../constants/phone-type';
+import { addressTypes } from '../constants/address-type';
+import { linesOfBusiness } from '../constants/line-of-business';
+import { contactStatuses } from '../constants/contact-status';
+import { sources } from '../constants/source';
+import { DropdownOption } from '../../ui/model/dropdown-option';
+import { DropdownService } from '../../ui/service/dropdown.service';
 
 @Component({
   selector: 'app-view-contact',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './view-contact.component.html',
   styleUrls: ['./view-contact.component.css']
 })
 export class ViewContactComponent implements OnInit {
 
+  availableAddressTypes: DropdownOption[] = addressTypes;
+  availablePhoneTypes: DropdownOption[] = phoneTypes;
+  availableLobTypes: DropdownOption[] = linesOfBusiness;
+  availableContactStatuses: DropdownOption[] = contactStatuses;
+  availableSources: DropdownOption[] = sources;
+
   loading: boolean = true;
   contact: Contact = {} as Contact;
 
   constructor(private route: ActivatedRoute, private contactService: ContactService,
-    private alertService: AlertService) { }
+    private alertService: AlertService, private router: Router, private dropDownService: DropdownService) { }
 
   ngOnInit(): void {
     let contact$ = this.route.queryParamMap.pipe(
@@ -52,5 +66,10 @@ export class ViewContactComponent implements OnInit {
     }
 
     this.alertService.error(alertMessage);
+  }
+
+  editContact() {
+    let route = '/contacts/edit-contact';
+    this.router.navigate([route], { queryParams: { id: this.contact.id } });
   }
 }
